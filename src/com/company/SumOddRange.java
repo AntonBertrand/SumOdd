fec97c261c3a30ed6c3474fcdcac45684b2cc07f
Anton <antonbertrand@hotmail.com>
2021-07-07 16:31:14 +0100

    Complete Coding Challenge

---
+++ src/com/company/SumOddRange.java
@@ -0,0 +1,37 @@
+package com.company;
+
+public class SumOddRange {
+
+    public static boolean isOdd (int number) {
+
+        if (number <= 0) {
+            return false;
+        }
+
+        if (number % 2 == 00) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public static int sumOdd (int start, int end) {
+
+        int sum = 0;
+
+        if ((end < start) || (start <= 0) || (end <= 0)) {
+            return -1;
+        }
+
+        for (int i = start; i <= end; i++) {
+
+
+            if (isOdd(i)) {
+                sum = sum + i;
+            }
+
+        }
+        return sum;
+    }
+
+}
